a87fb8a9ffb5aa159778d92b39ca1c96f939cca6
HyeRyeongYoon <dbs123457@gmail.com>
2025-07-21 22:09:22 +0900

    [수정] eslint 적용

--- src/utils/loadSVGPathProperties.ts
+++
@@ -1,7 +0,0 @@
-// src/utils/loadSVGPathProperties.ts
-export function loadSVGPathPropertiesCJS() {
-    // require 사용 (Turbo는 CJS를 static으로 분석 가능)
-
-    const { SVGPathProperties } = require("svg-path-properties");
-    return SVGPathProperties;
-}
